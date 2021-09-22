d2e5bbb5d2d4fa3792d93be2575428717738eb5d
Vigneshvj2503 <vignesh.oofl2503@gmail.com>
2021-09-22 15:31:56 +0530

    A Simple Node.js app

--- trivial_node.js_app/app.js
+++ trivial_node.js_app/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const os = require('os');
 
-console.log("Kubia Server Starting ...");
+console.log("Kubia Server Starting da...");
 
 var handler = function(request, response) {
 	console.log("Received request from " + request.connection.remoteAddress);
